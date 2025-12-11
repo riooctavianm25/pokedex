aa9d948c785f316f66cf664060b090c00de643e2
riooctavianm <riooctavianm@outlook.com>
2025-12-11 09:12:30 +0700

    update detail page

--- lib/Detail_page.dart
+++ lib/Detail_page.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatefulWidget {
-  const DetailPage({super.key});
+  final String imageUrl;
+  final String name;
+  final String type;
+  final int id;
+  
+  const DetailPage({
+    super.key,
+    required this.id, 
+    required this.imageUrl, 
+    required this.name, 
+    required this.type
+    });
 
   @override
   State<DetailPage> createState() => _DetailPageState();
@@ -11,12 +22,16 @@ class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            
-          ],
-        )),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Column(
+            children: [
+              Icon(Icons.arrow_back)
+            ]
+          ),
+        ),
+      ),
     );
   }
 }
