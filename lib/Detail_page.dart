d86e0f9e6355226337f54b9aacd655fb34c4e359
riooctavianm <riooctavianm@outlook.com>
2025-12-11 12:57:48 +0700

    update detail page

--- lib/Detail_page.dart
+++ lib/Detail_page.dart
@@ -11,7 +11,7 @@ class DetailPage extends StatefulWidget {
     required this.id, 
     required this.imageUrl, 
     required this.name, 
-    required this.type
+    required this.type,
     });
 
   @override
@@ -22,16 +22,31 @@ class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(40.0),
-          child: Column(
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
             children: [
-              Icon(Icons.arrow_back)
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      onPressed: () => Navigator.pop(context) 
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Text(widget.name, style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+                      Text(widget.type, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)) 
+                    ],
+                  )
+                ],
+              )
             ]
           ),
         ),
-      ),
     );
   }
 }
