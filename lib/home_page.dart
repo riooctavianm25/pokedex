3d474000b76984f4e47cfcb3af9bdd543f9c6542
riooctavianm <riooctavianm@outlook.com>
2025-12-11 13:30:08 +0700

    update home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -107,7 +107,6 @@ class _HomePageState extends State<HomePage> {
                   imageUrl: dummyPokemonList[index].imageurl, 
                   name: dummyPokemonList[index].name, 
                   type: dummyPokemonList[index].type, 
-                  id: dummyPokemonList[index].id,
                   ),
                 ),
               );
